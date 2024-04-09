821625fbe56bc8e48344fa95597c5c6194444bce
birdkron <js8215001@gmail.com>
2024-04-09 07:41:58 -0600

    Initial commit

---
+++ app/src/main/java/com/example/agendaudb/Perfil/Perfil_Usuario.java
@@ -0,0 +1,16 @@
+package com.example.agendaudb.Perfil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.agendaudb.R;
+
+public class Perfil_Usuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_usuario);
+    }
+}
